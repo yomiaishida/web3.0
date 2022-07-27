38cadbde0f25ab27331d79c91b5caa1da86c4ca7
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-27 17:13:16 +0100

    Preparing to deploy smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/2okFlffFbOc_iOxu3GsA11VrS4Z5WUZW
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/2okFlffFbOc_iOxu3GsA11VrS4Z5WUZW",
+      accounts: [
+        "8fa5d4781735d255fbc7ae3f00822232ffd2d73b31bf043fc746e4bead23eafd",
+      ],
+    },
+  },
 };
