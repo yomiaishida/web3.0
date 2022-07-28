6a891a944c8ff4c657bb1cd525dc37fcb078558a
Abayomi Aishida <yomicorperate@gmail.com>
2022-07-28 04:51:48 +0100

    React transaction context setup

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
+
+import { TransactionContext } from "../context/TransactionContext";
 
 import { Loader } from "./";
 
@@ -20,6 +22,8 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
+  const { value } = useContext(TransactionContext);
+  console.log("text");
   const connectWallet = () => {};
 
   const handleSubmit = () => {};
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transaction.json";
 
-export const contractAbi = abi.abi;
+export const contractABI = abi.abi;
 export const contractAddress = "0x5F44Ece19590c0664EF0160F162C238Fc95c0e6b";
