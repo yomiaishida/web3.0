37aab08b5aaf648fff0d9940f389fcdd54f59e21
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-11 00:17:33 +0100

    Display transaction card with dummy data

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,8 +4,8 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { TransactionContext } from "../context/TransactionContext";
-
 import { Loader } from "./";
+import { shortenAddress } from "../utils/shortenAddress";
 
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -83,7 +83,9 @@ const Welcome = () => {
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
-                <p className="text-white font-light text-sm mt-0.2">Address</p>
+                <p className="text-white font-light text-sm mt-0.2">
+                  {shortenAddress(currentAccount)}
+                </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
                 </p>
