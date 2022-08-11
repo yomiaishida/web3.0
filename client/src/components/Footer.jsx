49e203a7a81bf11830171e3c95272887d00982ab
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-11 01:10:38 +0100

    Fix: typo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../../images/images/logo.png";
 const Footer = () => {
   return (
     <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
-      <div className="w-full flex sm-flex-row flex-col justify-between items-center my-4">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
         <div className="flex flex-[0.5] justify-center items-center">
           <img src={logo} alt="logo" className="w-32" />
         </div>
