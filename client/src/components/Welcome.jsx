03dd36fc0c20b73579d7bc09a205c3e38dd9047e
Abayomi Aishida <yomicorperate@gmail.com>
2022-08-12 00:53:51 +0100

    Is loading spinner

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -28,6 +28,7 @@ const Welcome = () => {
     formData,
     sendTransaction,
     handleChange,
+    isLoading,
   } = useContext(TransactionContext);
 
   const handleSubmit = (e) => {
@@ -121,7 +122,7 @@ const Welcome = () => {
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {false ? (
+            {isLoading ? (
               <Loader />
             ) : (
               <button
